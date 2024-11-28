89ec998d62fe663a070b62f189b855ae3c041fa6
Steven <stevengf14@gmail.com>
2024-11-27 23:37:04 -0500

    Creating our first microservice with Spring Boot and Cloud

---
+++ msvc-users/src/test/java/ec/com/learning/sprongcloud/msvc/users/MsvcUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.learning.sprongcloud.msvc.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
