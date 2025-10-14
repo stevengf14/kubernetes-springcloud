2ad33c6ffd9eff8ad0b03f29c762b7488db5870c
Steven <stevengf14@gmail.com>
2025-10-13 23:27:45 -0500

    Implementing HTTP communication methods in the servide

--- msvc-courses/src/main/java/ec/com/learning/sprongcloud/msvc/courses/services/CourseServiceImpl.java
+++ msvc-courses/src/main/java/ec/com/learning/sprongcloud/msvc/courses/services/CourseServiceImpl.java
@@ -3,6 +3,7 @@ package ec.com.learning.sprongcloud.msvc.courses.services;
 import ec.com.learning.sprongcloud.msvc.courses.clients.UserClientRest;
 import ec.com.learning.sprongcloud.msvc.courses.models.User;
 import ec.com.learning.sprongcloud.msvc.courses.models.entity.Course;
+import ec.com.learning.sprongcloud.msvc.courses.models.entity.CourseUser;
 import ec.com.learning.sprongcloud.msvc.courses.repositories.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -45,17 +46,56 @@ public class CourseServiceImpl implements CourseService {
     }
 
     @Override
+    @Transactional
     public Optional<User> assignUser(User user, Long courseId) {
+        Optional<Course> o = repository.findById(courseId);
+        if (o.isPresent()) {
+            User userMsvc = client.findById(user.getId());
+
+            Course course = o.get();
+            CourseUser courseUser = new CourseUser();
+            courseUser.setUserId(userMsvc.getId());
+
+            course.addCourseUser(courseUser);
+            repository.save(course);
+            return Optional.of(userMsvc);
+        }
         return Optional.empty();
     }
 
     @Override
+    @Transactional
     public Optional<User> createUser(User user, Long courseId) {
+        Optional<Course> o = repository.findById(courseId);
+        if (o.isPresent()) {
+            User newUserMsvc = client.save(user);
+
+            Course course = o.get();
+            CourseUser courseUser = new CourseUser();
+            courseUser.setUserId(newUserMsvc.getId());
+
+            course.addCourseUser(courseUser);
+            repository.save(course);
+            return Optional.of(newUserMsvc);
+        }
         return Optional.empty();
     }
 
     @Override
+    @Transactional
     public Optional<User> removeUser(User user, Long courseId) {
+        Optional<Course> o = repository.findById(courseId);
+        if (o.isPresent()) {
+            User userMsvc = client.findById(user.getId());
+
+            Course course = o.get();
+            CourseUser courseUser = new CourseUser();
+            courseUser.setUserId(userMsvc.getId());
+
+            course.removeCourseUser(courseUser);
+            repository.save(course);
+            return Optional.of(userMsvc);
+        }
         return Optional.empty();
     }
 }
